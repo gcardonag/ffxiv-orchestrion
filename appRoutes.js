2e69a49e6efef0e1f03623522d0881b34877f426
gcardonag <gcardonagarcia@gmail.com>
2017-04-30 01:32:46 -0400

    Added categories and updated roll data to 3.57.
    Data has been updated to include a new category and categoryid field.
    Category templates have been created to show rolls by category.
    App logic has been updated to include controllers for each category. (This area can probably be cleaned up due to copy/pasted code)
    The Pa-Paya roll id was changed from 0 to 145 (last ID at the time) to remove issues caching data.
    Some of the older rolls have had their data updated with extra detail.
    Credit for data catching up to patch 3.5 has been added.
    Credit for adding categories has been added.

--- appRoutes.js
+++ appRoutes.js
@@ -33,4 +33,197 @@ angular.module('orchestrionApp.routes', [])
       }
     }
   });
+
+  $stateProvider
+  .state('locales', {
+    url: '/locales',
+    templateUrl: 'templates/locales.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('dungeons', {
+    url: '/dungeons',
+    templateUrl: 'templates/dungeons.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('primal_battles', {
+    url: '/primal_battles',
+    templateUrl: 'templates/primal_battles.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('raids', {
+    url: '/raids',
+    templateUrl: 'templates/raids.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('others', {
+    url: '/others',
+    templateUrl: 'templates/others.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('seasonal', {
+    url: '/seasonal',
+    templateUrl: 'templates/seasonal.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
 });
