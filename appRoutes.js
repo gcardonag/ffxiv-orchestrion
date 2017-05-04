de6a2f3818e086c7612012a2d59ba1f70fc3c5a6
gcardonag <gcardonagarcia@gmail.com>
2017-05-03 21:02:06 -0400

    Adding states for other categories

--- appRoutes.js
+++ appRoutes.js
@@ -66,4 +66,164 @@ angular.module('orchestrionApp.routes', [])
     }
   });
 
+  $stateProvider
+  .state('dungeons', {
+    url: '',
+    templateUrl: 'templates/dungeons.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('primal_battles', {
+    url: '',
+    templateUrl: 'templates/primal_battles.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('raids', {
+    url: '',
+    templateUrl: 'templates/raids.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('others', {
+    url: '',
+    templateUrl: 'templates/others.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
+  $stateProvider
+  .state('seasonal', {
+    url: '',
+    templateUrl: 'templates/seasonal.html',
+    controller: 'appController',
+    resolve: {
+      data: function ($http, $localStorage) {
+        if(!$localStorage['ffxivOrchestrion']) {
+          $localStorage['ffxivOrchestrion'] = {};
+        }
+        return $http.get('data/rollData.json').then(function(response) {
+          var rollData = response.data;
+
+          // update roll data checked data
+          _.forEach(rollData, function (roll, rollIndex) {
+            rollData[rollIndex].checked = _.get($localStorage.ffxivOrchestrion[rollIndex], 'checked');
+
+            if (_.has(roll, 'source.recipeItems') && roll.source.recipeItems) {
+              _.forEach(roll.source.recipeItems, function (ingredient, ingredientIndex) {
+                if ($localStorage.ffxivOrchestrion[rollIndex]) {
+                  rollData[rollIndex].source.recipeItems[ingredientIndex].checked = $localStorage.ffxivOrchestrion[rollIndex].ingredients[ingredientIndex] || false;
+                }
+              });
+            }
+          });
+
+          return rollData;
+        });
+      }
+    }
+  });
+
 });
